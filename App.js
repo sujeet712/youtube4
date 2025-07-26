da91e2e052db32bde78cf1fd83dae5520fa664c3
Sujeet Kumar <38582345+sujeet712@users.noreply.github.com>
2025-07-26 16:10:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import "./App.css";
+import Body from "./components/Body";
+import Head from "./components/Head";
+import { Provider } from "react-redux";
+import store from "./utils/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+
+const appRouter = createBrowserRouter(
+  [{
+    path: "/",
+    element: <Body />,
+    children:[    
+      {
+        path: "/",
+        element: <MainContainer/>
+      },
+      {
+        path: "watch",
+        element: <WatchPage/>
+      },
+
+    ]
+  }]
+)
+function App() {
+  return (
+    <Provider store={store}>
+      <div>
+        <Head />
+        <RouterProvider router={appRouter} />
+        {/*
+  Head
+  Body
+    SideBar
+      MenuItems
+ 
+       */}
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
